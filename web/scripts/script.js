dc413ba16c8767a4d40bcfa2bfc3af4448d55b79
TBrown84 <43593578+TBrown84@users.noreply.github.com>
2019-03-05 18:44:25 -0600

    sdxfdghjkii

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,27 +1,4 @@
-// When the user scrolls the page, execute myFunction 
-// window.onscroll = function() {myFunction()};
 
-// Get the navbar
-// var searchbar = document.getElementByClass("searchbar");
-
-// Get the offset position of the navbar
-// var sticky = searchbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     searchbar.classList.add("sticky")
-//   } else {
-//     searchbar.classList.remove("sticky");
-//   }
-// }
-
-// $(".flex-container").onscroll(function(){
-//   var x = $("flex-container").offset();
-//   if (x === 0) {
-//     $(".flex-container").css($(".sticky-container"))
-//   }
-// })
 
 const filter = document.querySelector("#searchbar");
 const topOfFilter = filter.offsetTop;
